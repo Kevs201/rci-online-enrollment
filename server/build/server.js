88ae2e95a78ea7d504509c01dd331b17b0165741
Kevs201 <sankevin535@gmail.com>
2025-02-02 16:26:43 +0800

    don

--- server/build/server.js
+++ server/build/server.js
@@ -1,32 +1,23 @@
 "use strict";
-
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
 Object.defineProperty(exports, "__esModule", { value: true });
-
-// Import required modules
-const http_1 = __importDefault(require("http"));
 const app_1 = require("./app");
 const cloudinary_1 = require("cloudinary");
+const http_1 = __importDefault(require("http"));
 const db_1 = __importDefault(require("./utils/db"));
 const socketServer_1 = require("./socketServer");
 require("dotenv").config();
-
-// Cloudinary configuration
+const server = http_1.default.createServer(app_1.app);
+// cloudinary config
 cloudinary_1.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_SECRET_KEY,
 });
-
-// Initialize socket server
 (0, socketServer_1.initSocketServer)(server);
-
-// Create and start server
-const server = http_1.default.createServer(app_1.app);
-
+// create server
 server.listen(process.env.PORT, () => {
     console.log(`Server is connected with port ${process.env.PORT}`);
     (0, db_1.default)();
