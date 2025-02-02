c0f3f2ffa5271ece9c3d1682e9ee44af52609851
Kevs201 <sankevin535@gmail.com>
2025-02-02 18:15:46 +0800

    fix gget user none

--- server/build/app.js
+++ server/build/app.js
@@ -24,7 +24,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross-origin resource sharing
 exports.app.use((0, cors_1.default)({
-    origin: ['https://rci-online-enrollment-client.vercel.app'], // Fix here: remove trailing slash
+    origin: ['https://rci-online-enrollment-client.vercel.app/'], // Fix here: remove trailing slash
     credentials: true,
 }));
 
