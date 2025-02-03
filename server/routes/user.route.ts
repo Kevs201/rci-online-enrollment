80e17d85e0b825ea6acaf5dcd6314351a3cd001b
Kevs201 <sankevin535@gmail.com>
2025-02-03 10:37:02 +0800

    fix update all update

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -21,25 +21,24 @@ userRouter.post("/registration", registrationUser);
 
 userRouter.post("/activate-user", activateUser);
 
-userRouter.post("/login"  ,loginUser);
+userRouter.post("/login" , loginUser);
 
 userRouter.get("/logout", isAutheticated, logoutUser);
 
 userRouter.get("/refresh", updateAccessToken);
 
-userRouter.get("/me",updateAccessToken, isAutheticated, getUserInfo);
+userRouter.get("/me", isAutheticated, getUserInfo);
 
 userRouter.post("/social-auth", socialAuth);
 
-userRouter.put("/update-user-info",updateAccessToken, isAutheticated, updateUserInfo);
+userRouter.put("/update-user-info", isAutheticated, updateUserInfo);
 
-userRouter.put("/update-user-password",updateAccessToken, isAutheticated, updatePassword);
+userRouter.put("/update-user-password", isAutheticated, updatePassword);
 
-userRouter.put("/update-user-avatar",updateAccessToken, isAutheticated, updateProfilePicture);
+userRouter.put("/update-user-avatar", isAutheticated, updateProfilePicture);
 
 userRouter.get(
   "/get-users",
-  updateAccessToken,
   isAutheticated,
   authorizeRoles("admin"),
   getAllusers
@@ -47,7 +46,6 @@ userRouter.get(
 
 userRouter.put(
   "/update-users",
-  updateAccessToken,
   isAutheticated,
   authorizeRoles("admin"),
   updateUserRole
@@ -55,7 +53,6 @@ userRouter.put(
 
 userRouter.delete(
   `/delete-user/:id`,
-  updateAccessToken,
   isAutheticated,
   authorizeRoles("admin"),
   deleteUser
