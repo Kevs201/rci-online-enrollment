a12507e2d2cb8994c14b79d5ce2957ebcc7b643c
Kevs201 <sankevin535@gmail.com>
2025-02-04 21:11:01 +0800

    finall update the route for get-user

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -27,7 +27,7 @@ userRouter.get("/logout", isAutheticated, logoutUser);
 
 userRouter.get("/refresh", updateAccessToken);
 
-userRouter.get("/me" ,isAutheticated, updateAccessToken, getUserInfo);
+userRouter.get("/me" ,isAutheticated, getUserInfo);
 
 userRouter.post("/social-auth", socialAuth);
 
